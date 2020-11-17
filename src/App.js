f6a4ff4a605b7a09173dcd4d703e1b1dcd46dc90
Paulo Ferrari <pf.devcon@gmail.com>
2020-11-17 01:41:09 -0300

    added methods to list, create and delete repositories

--- src/App.js
+++ src/App.js
@@ -1,27 +1,73 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    handleGetRepositories();
+  }, []);
+
+  async function handleGetRepositories() {
+    try {
+      const response = await api.get("/repositories");
+      setRepositories(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   async function handleAddRepository() {
-    // TODO
+    let repository = {
+      title: `ReactJS concepts ${Date.now()}`,
+      url: "https://github.com/PauloFerrariDev/gostack-reactjs-concepts",
+      techs: ["ReactJS", "JavaScript"],
+    };
+
+    try {
+      const response = await api.post("/repositories", repository);
+      repository = { ...response.data };
+      setRepositories([...repositories, repository]);
+    } catch (error) {
+      console.error(error);
+    }
+
+    console.log("Created repository:", repository);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`/repositories/${id}`);
+
+      const repositoriesTemp = repositories.filter(
+        (repository) => repository.id !== id
+      );
+
+      setRepositories(repositoriesTemp);
+    } catch (error) {
+      console.error(error);
+    }
+
+    console.log("Repository removed.");
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
+
+      <br />
 
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
